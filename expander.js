b6834e41925b74931183151899fe86b80262d867
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-02 19:39:45 -0400

    testing

--- expander.js
+++ expander.js
@@ -36,11 +36,8 @@
 			}
 			if (style = dom.currentStyle) {
 				console.log(style);
-				for (prop in style) {
-					console.log(prop + ": " + style[prop]);
-					dest[prop] = style[prop];
-				}
-				return this.css(dest);
+				
+				$(this).css(style);
 			}
 			if (style = dom.style) {
 				console.log(style);

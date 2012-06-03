ddd694c05731b8c5e1ae1d3410305d4c4dfd2c0a
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-03 16:54:05 -0400

    cross-browser fixes

--- expander.js
+++ expander.js
@@ -16,36 +16,47 @@
 						
 			// Create hidden div with all the same styles as textarea, plus a few extra needed things
 
-			var	box = $('<div class="box"></div>');
+			var	box = $('<div class="box"></div>'),
+				styles = {};
 			
 			if(window.getComputedStyle){
 				var camelize = function(a,b){ return b.toUpperCase(); }
-				var returns = {};
 				var style = window.getComputedStyle(el, null);
 				for(var i = 0, l = style.length; i < l; i++){
 					var prop = style[i];
 					var camel = prop.replace(/\-([a-z])/, camelize);
 					var val = style.getPropertyValue(prop);
-					returns[camel] = val;
+					styles[camel] = val;
 				}
-				$(box).css(returns);
 			}
+			else if (el.currentStyle) {
+				var style = el.currentStyle,
+					re = /(\-([a-z]){1})/g,
+					toDash = function(str) { return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();}); };
+				for (var prop in style) {
+					if (prop == 'float') prop = 'styleFloat';
+					if (re.test(prop)) prop = prop.replace(re, function() { return arguments[2].toUpperCase(); });
+					try { if(style[prop]) styles[toDash(prop)] = style[prop]; }
+					catch(err) {}
+				}
+			}
+			
+			$(box).css(styles)
 			
 			$(box).css({
 				"height"		: "auto",
 				"position"   	: "absolute",
 				"visibility" 	: "hidden",
 				"left"			: "-9999px",
-				"width" 		: $(el).width(),
-				"line-height"	: $(el).css("line-height"),
-				"font-size"		: $(el).css("font-size"),
-				"font-family"	: $(el).css("font-family")
-			}).insertAfter(el);
+				"width" 		: $(el).width()
+			});
+			
+			$(box).insertAfter(el);
 			
 			// Populate hidden div with text as you tpe and resize textarea based on height
 			
 			$(el).bind("input keydown keyup change", function() {
-				$(box).html(el.value.replace(/(\r\n|\r|\n)$/g,"<br /><br />").replace(/\r?\n|\r/g, "<br />"));
+				$(box).html(el.value.replace(/(\r\n|\r|\n)$/g,"<br /><br />|").replace(/\r?\n|\r/g, "<br />|"));
 				$(el).height($(box).height());
 			});
 			

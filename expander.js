d44c5d476f45e789fb2d68202c7c074e047b6ccb
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-04 00:11:29 -0400

    cross-browser fixes

--- expander.js
+++ expander.js
@@ -2,8 +2,6 @@
 
 	$.fn.expander = function() {
 		
-		// Set up expanding for each element
-		
 		this.each(function(i,el) {
 			
 			// Set requisite styles on textarea to make this work
@@ -11,31 +9,32 @@
 			$(el).css({
 				"resize" 		: "none",
 				"word-wrap"		: "break-word",
-				"white-space"	: "pre-wrap"
+				"white-space"	: "pre-wrap",
+				"overflow"		: "hidden"
 			});
 						
 			// Create hidden div with all the same styles as textarea, plus a few extra needed things
 
-			var	box = $('<div class="box"></div>'), styles = {};
+			var	box			= $('<div class="box"></div>'),
+				styles		= {},
+				re			= /(\-([a-z]){1})/g,
+				camelize	= function(a,b){ return b.toUpperCase(); },
+				toDash		= function(str) { return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();}); };
 			
 			if(window.getComputedStyle){
-				var camelize = function(a,b){ return b.toUpperCase(); }
-				var style = window.getComputedStyle(el, null);
+				var style =style = window.getComputedStyle(el, null);
 				for(var i = 0, l = style.length; i < l; i++){
 					var prop = style[i];
-					var camel = prop.replace(/\-([a-z])/, camelize);
+					var camel = prop.replace(re, camelize);
 					var val = style.getPropertyValue(prop);
 					styles[camel] = val;
 				}
 			}
 			else if (el.currentStyle) {
-				var style = el.currentStyle,
-					re = /(\-([a-z]){1})/g,
-					toDash = function(str) { return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();}); };
+				var style = el.currentStyle;
 				for (var prop in style) {
-					if (prop == 'float') prop = 'styleFloat';
-					if (re.test(prop)) prop = prop.replace(re, function() { return arguments[2].toUpperCase(); });
-					try { if(style[prop]) styles[toDash(prop)] = style[prop]; }
+					prop = prop.replace(re, camelize);
+					try { styles[toDash(prop)] = style[prop]; }
 					catch(err) {}
 				}
 			}
@@ -45,16 +44,23 @@
 				"position"   	: "absolute",
 				"visibility" 	: "hidden",
 				"left"			: "-9999px",
+				"margin"		: $(el).css("margin"),
 				"width" 		: $(el).width()
 			}).insertAfter(el);
 			
 			// Populate hidden div with text as you tpe and resize textarea based on height
 			
-			$(el).bind("input keydown keyup change", function() {
-				if ($.browser.msie && $.browser.version <= 7) $(box).html(el.value.replace(/(\r\n|\r|\n)$/g,"<br /><br />|").replace(/\r?\n|\r/g, "<br />|"));
-				else $(box).html(el.value.replace(/(\r\n|\r|\n)$/g,"<br /><br />").replace(/\r?\n|\r/g, "<br />"));
+			var grow = function() {
+				var value = el.value.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+				if ($.browser.msie && $.browser.version <= 7)
+					 $(box).html(value.replace(/(\r\n|\r|\n)$/g,"<br /><br />|").replace(/\r?\n|\r/g, "<br />|"));
+				else $(box).html(value.replace(/(\r\n|\r|\n)$/g,"<br /><br />").replace(/\r?\n|\r/g, "<br />"));
 				$(el).height($(box).height());
-			});
+			};
+			
+			$(el).bind("input keydown keyup change", grow);
+			
+			grow();
 			
 			// In case the textarea is fluid and window gets resized 
 			
@@ -64,6 +70,7 @@
 			});
 			
 		});
+
 	};
 	
 })( jQuery );
